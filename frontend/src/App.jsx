69d9f50607a223200418e9f92d6229cc72480d26
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-08-21 11:03:08 -0300

    front and backend connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./Components/Form";
 import Table from "./Components/Table";
 
 const App = () => {
   const [buttonVisible, setButtonVisible] = useState(true);
+  const [data, setData] = useState([]);
+  const api = "http://localhost:8080/products";
+
+  // useEffect(() => {
+  //   fetch(api)
+  //     .then((ret) => ret.json())
+  //     .then((retConv) => setData(retConv));
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
+      <p>{JSON.stringify(data)}</p>
       <h1>Spring-React App</h1>
       <Form buttonVisibility={buttonVisible} />
       <Table />
-
-      {console.log(buttonVisible)}
     </div>
   );
 };
