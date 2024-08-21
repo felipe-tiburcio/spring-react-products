4f19d8190f5fa6f8498ee5596a26096c91b0bd8b
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-08-21 09:24:08 -0300

    conditional buttons rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./Components/Form";
 import Table from "./Components/Table";
 
 const App = () => {
+  const [buttonVisible, setButtonVisible] = useState(true);
+
   return (
     <div>
       <h1>Spring-React App</h1>
-      <Form />
+      <Form buttonVisibility={buttonVisible} />
       <Table />
+
+      {console.log(buttonVisible)}
     </div>
   );
 };
--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,13 +1,18 @@
-const Form = () => {
+const Form = ({ buttonVisibility }) => {
   return (
     <form action="">
       <input className="form-control" placeholder="Name" />
       <input className="form-control" placeholder="Brand" />
 
-      <button className="btn btn-primary">Save</button>
-      <button className="btn btn-warning">Edit</button>
-      <button className="btn btn-danger">Delete</button>
-      <button className="btn btn-secondary">Cancel</button>
+      {buttonVisibility ? (
+        <button className="btn btn-primary">Save</button>
+      ) : (
+        <div>
+          <button className="btn btn-warning">Edit</button>
+          <button className="btn btn-danger">Delete</button>
+          <button className="btn btn-secondary">Cancel</button>
+        </div>
+      )}
     </form>
   );
 };
